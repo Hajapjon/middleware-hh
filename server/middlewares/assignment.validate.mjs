5f7af4f7a885c0b1bbec8357bc086cb31233b195
nichalada <nichaladasuphachayanon@gmail.com>
2025-04-10 14:49:05 +0700

    feat: create middleware for validating assignment post request

---
+++ server/middlewares/assignment.validate.mjs
@@ -0,0 +1,37 @@
+export const validateCreateAssignment = (req, res, next) => {
+    const { title, content, category, email } = req.body;
+  
+    if (!title) {
+      return res.status(400).json({
+        message: "Title is required.",
+      });
+    }
+  
+    if (!content || content.length < 500 || content.length > 1000) {
+      return res.status(400).json({
+        message: "Content must be between 500 and 1000 characters.",
+      });
+    }
+  
+    const allowedCategories = ["Math", "English", "Biology"];
+    if (!category || !allowedCategories.includes(category)) {
+      return res.status(400).json({
+        message: "Category must be one of: Math, English, or Biology.",
+      });
+    }
+  
+    if (!email) {
+      return res.status(400).json({
+        message: "Email is required.",
+      });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        message: "Email format is invalid.",
+      });
+    }
+    next();
+  };
+  
+  
